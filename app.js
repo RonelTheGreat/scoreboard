3c96aebc78a1752c60de439fe2c72f9dded760d9
ronel carlo berino <ronelcarlo.berino@gmail.com>
2019-06-30 15:02:33 +0800

    added database

--- app.js
+++ app.js
@@ -1,16 +1,31 @@
-const express = require('express'),
-      app     = express(),
-      http    = require('http').Server(app),
-      io      = require('socket.io')(http),
-      PORT    = process.env.PORT || 3000;
+const mongoose  = require('mongoose'),
+      express   = require('express'),
+      Sboard    = require('./models/scoreboard'),
+      app       = express(),
+      http      = require('http').Server(app),
+      io        = require('socket.io')(http),
+      PORT      = process.env.PORT || 3000;
+
 
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
 
+// MONGODB CONNECTION
+const DB_URL = 'mongodb+srv://admin123:admin123@cluster0-brxlt.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(DB_URL);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+
 app.get('/', (req, res) => {
-    res.render('index');
+    Sboard.find({}, (err, scoreboard) => {
+        if (err) return console.log('Can\'t find instance of scoreboard ...');
+
+        res.render('index', {scoreboard : scoreboard[0]}); 
+    })
 })
 
 io.on('connection', (socket) => {
